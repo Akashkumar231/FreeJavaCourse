e50ea323396f3dadc8b9fae0218b9b53a377c7d3
Akashkumar231 <101165107+Akashkumar231@users.noreply.github.com>
2022-09-05 19:32:03 +0530

    Add files via upload

---
+++ LanguageFundamentals/JavaPrograms/Identifier.java
@@ -0,0 +1,12 @@
+package DurgaSoftwareSolution.LanguageFundamentals;
+
+public class Identifier {
+    public static void main(String [] args){
+        System.out.println("Hello , World");
+        int main=5;
+        int Identifier;
+        int Runnable;
+//        Note :- "JAVA IS CASE SENSITIVE LANGUAGE"
+//        float float; :-This line will give compile time error because we can use any other name except  reserved word in java.
+       }
+}
